02eb0d989101fe126f666e350f8e85815b213e47
Rolando Oc <admi.jmb@gmail.com>
2022-07-21 08:58:58 -0500

    03 | Basic CRUD API

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
+import { TaskDTO } from './dto/task.dto';
+import { ITask } from './interfaces/task.interface';
 import { TaskService } from './task.service';
 
 @Controller('api/v1/task')
@@ -7,52 +19,39 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  store(@Req() req: Request, @Res() res: Response) {
-    const data = {
-      name: req.body.name,
-      method: `${req.method}`,
-    };
-
+  store(@Body() taskDTO: TaskDTO, @Res() res: Response): Response<ITask> {
+    const data = this.taskService.create(taskDTO);
     return res.json(data);
   }
 
   @Get()
-  index(@Req() req: Request, @Res() res: Response) {
-    const data = {
-      name: 'Nian Dewei',
-      method: `${req.method}`,
-    };
-
+  index(@Res() res: Response): Response<ITask[]> {
+    const data = this.taskService.all();
     return res.json(data);
   }
 
-  @Get(':id')
-  show(@Req() req: Request) {
-    const data = {
-      _id: req.params.id,
-      method: `${req.method}`,
-    };
-
-    return data;
+  @Get(':id') //isObjectId
+  show(@Param('id') id: string, @Res() res: Response): Response<ITask> {
+    const data = this.taskService.findById(id);
+    return res.json(data);
   }
 
   @Put(':id')
-  update(@Req() req: Request, @Res() res: Response) {
-    const data = {
-      _id: req.params.id,
-      method: `${req.method}`,
-    };
+  update(@Req() req: Request, @Res() res: Response): Response<ITask> {
+    const taskId = req.params.id;
+    const taskToUpdate = req.body;
 
-    return res.status(200).json(data);
+    const updatedTask = this.taskService.findByIdAndUpdate(
+      taskId,
+      taskToUpdate,
+    );
+
+    return res.json(updatedTask);
   }
 
   @Delete(':id')
-  destroy(@Req() req: Request, @Res() res: Response) {
-    const data = {
-      _id: req.params.id,
-      method: `${req.method}`,
-    };
-
-    return res.status(200).json(data);
+  destroy(@Param('id') id: string, @Res() res: Response): Response<string> {
+    const messageResponse = this.taskService.destroyById(id);
+    return res.json({ message: messageResponse });
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { TaskDTO } from './dto/task.dto';
+import { ITask } from './interfaces/task.interface';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: ITask[] = [];
+
+  create(taskDTO: TaskDTO): ITask {
+    const task: ITask = {
+      id: uuidv4(),
+      description: taskDTO.description,
+      isDone: taskDTO.isDone,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  all(): ITask[] {
+    return this.tasks;
+  }
+
+  findById(id: string): ITask {
+    const data = this.tasks.find((task) => task.id === id);
+    return data;
+  }
+
+  findByIdAndUpdate(id: string, taskDTO: TaskDTO): ITask {
+    const newTask = { id, ...taskDTO };
+    this.tasks = this.tasks.map((task) => (task.id === id ? newTask : task));
+    return newTask;
+  }
+
+  destroyById(id: string): string {
+    const messageResponse = 'Deleted Task';
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    return messageResponse;
+  }
+}
